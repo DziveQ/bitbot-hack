8b68e9e310f035f72f6ef0e451dbe3471c89cfd5
DziveQ <56726128+DziveQ@users.noreply.github.com>
2020-12-15 12:55:39 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 input.onButtonPressed(Button.A, function () {
     for (let indeks = 0; indeks <= 255; indeks++) {
         radio.setGroup(indeks)
+        serial.writeLine("Connected to " + indeks)
         for (let indeks2 = 0; indeks2 <= 255; indeks2++) {
             radio.sendNumber(indeks2)
+            serial.writeLine("Sent " + indeks2 + "to " + indeks)
         }
     }
 })
